957e1df3b6eb79dc23d682b70036cd1edc0f5175
hiitsketaki <98201093+hiitsketaki@users.noreply.github.com>
2022-10-18 21:07:06 +0530

    Day 3: Intro To Conditional Statements

---
+++ day_3.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+    /*conditions*/
+    if(N%2==0){
+        if(2<=N && N<=5){
+            cout<<"Not Weird";
+        }
+        else if(6<=N && N<=20){
+            cout<<"Weird";
+        }
+        else{
+            cout<<"Not Weird";
+        }
+    }
+    else{
+        cout<<"Weird";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
